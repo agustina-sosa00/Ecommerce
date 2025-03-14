c2b6bb3b94d1aa05bd38e920fd7fdda25c15bb6d
Agustina Sosa <agustinaayelensosa2@gmail.com>
2025-03-14 15:42:52 -0300

    agregando texto al hero

--- src/View/Hero/ImgHero.jsx
+++ src/View/Hero/ImgHero.jsx
@@ -3,7 +3,7 @@ import imgHero from '../../assets/imgHero.png';
 
 export const ImgHero = () => {
   return (
-    <div className="flex items-start justify-center w-full py-5">
+    <div className="flex items-center justify-center w-full py-5 lg:justify-start lg:items-start lg:ml-10">
       <img src={imgHero} alt="" className="w-1/2" />
     </div>
   );
