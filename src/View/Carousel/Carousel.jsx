cd73788ba0ee850f9c4e3042e57a7aa56e28f66a
Agustina Sosa <agustinaayelensosa2@gmail.com>
2025-05-08 21:49:54 -0300

    carrusel mapea datos y los muestra, falta estilos

--- src/View/Carousel/Carousel.jsx
+++ src/View/Carousel/Carousel.jsx
@@ -8,12 +8,29 @@ export const Carousel = () => {
   const prodByCat = useSelector((state) => state.products.productsByCat);
   console.log('prodByCat', prodByCat);
   return (
-    <div style={{ height: '100vh', width: '100%' }}>
-      <Swiper slidesPerView={1} loop style={{ height: '100%', width: '100%' }}>
-        <SwiperSlide>
-          <div>hola</div>
+    <Swiper
+      slidesPerView={1}
+      loop
+      className="contain-carousel"
+      // modules={[Autoplay]}
+      // autoplay={{ delay: 3000, disableOnInteraction: false }}
+    >
+      {prodByCat?.map((item, index) => (
+        <SwiperSlide key={index} className="flex flex-col w-full h-full ">
+          <div className="flex justify-center w-full py-10 ">
+            <h1 className="titleProd-carousel">featured products</h1>
+          </div>
+          <div className="flex w-full">
+            <div className="flex items-center justify-end w-1/2 h-full pr-5">
+              <img src={item[0].image} alt={item[0].title} className="w-2/5" />
+            </div>
+            <div className="w-1/2 h-full">
+              {' '}
+              <h1 className="font-title">{item[0].title}</h1>
+            </div>
+          </div>
         </SwiperSlide>
-      </Swiper>
-    </div>
+      ))}
+    </Swiper>
   );
 };
