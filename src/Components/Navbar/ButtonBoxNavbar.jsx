19e2fbb617061abac815c5bf165122bda6b35035
Agustina Sosa <agustinaayelensosa2@gmail.com>
2025-05-21 15:19:12 -0300

    navbar ajustado a 2xl

--- src/Components/Navbar/ButtonBoxNavbar.jsx
+++ src/Components/Navbar/ButtonBoxNavbar.jsx
@@ -24,10 +24,10 @@ export const ButtonBoxNavbar = () => {
 
   return (
     <div
-      className="items-center justify-center hidden w-1/3 h-20 lg:flex"
+      className="items-center justify-center hidden w-1/3 h-20 2xl:h-24 lg:flex"
       id="navbar-default"
     >
-      <ul className="flex flex-row items-center justify-center w-full h-20 gap-5 font-medium">
+      <ul className="flex flex-row items-center justify-center w-full h-20 gap-5 font-medium 2xl:h-24">
         {buttonBoxNavbar.map((e) =>
           e.type === 'scroll' ? (
             <ScrollLink
